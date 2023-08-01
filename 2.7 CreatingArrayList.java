9131b47bb1ba01138af2cd7df6e4592ae235f956
[your <tw841117@gmail.com>
2023-08-01 00:20:52 -0400

    upload 2.7 CreatingArrayList

---
+++ 2.7 CreatingArrayList.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+
+public class CreatingArrayList {
+
+	public static void main(String[] args) {
+
+		ArrayList<Integer> nums = new ArrayList<Integer>();
+		nums.add(1);
+		nums.add(3);
+		nums.add(5);
+		nums.add(7);
+	    System.out.println(nums);
+	    
+		ArrayList<String> cars = new ArrayList<String>();
+	    cars.add("Volvo");
+	    cars.add("BMW");
+	    cars.add("Ford");
+	    cars.add("Mazda");
+	    System.out.println(cars);
+	}
+
+}
