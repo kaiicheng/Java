c568c7091694c104c0ac20b433adad1dc311b992
[your <tw841117@gmail.com>
2023-08-03 12:58:37 -0400

    upload SizeOfAnArrayList

---
+++ SizeOfAnArrayList.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class SizeOfAnArrayList {
+
+	public static void main(String[] args) {
+
+		ArrayList<Integer> nums = new ArrayList<Integer>();
+		nums.add(1);
+		nums.add(3);
+		nums.add(5);
+		nums.add(7);
+	    System.out.println(nums);
+	    
+	    System.out.println(nums.size());
+	}
+}
