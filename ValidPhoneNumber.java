1ac8418bcccc8ecd535b18ed7798b73db8eaeece
[your <tw841117@gmail.com>
2023-08-20 00:36:28 -0400

    upload ValidPhoneNumber

---
+++ ValidPhoneNumber.java
@@ -0,0 +1,24 @@
+
+public class ValidPhoneNumber {
+	public static void main(String[] args) {
+	    
+		// random phone number
+	    String tel = "1234567891011";
+	    int len = tel.length();
+	    
+	    if(len!=11) System.out.println("Invid length!");
+	    
+	    for(int i= 0; i < len; i++){
+	    	
+	    	System.out.println(i);
+	    	
+	        // get every digit
+	        char cur = tel.charAt(i);
+	        
+	        if(cur <= '0' || cur >= '9'){
+	            System.out.println("Invalid input!");
+	            break;
+	        }
+	    }
+	}
+}
