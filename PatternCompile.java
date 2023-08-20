2187393d63801171f08c5d5306c2e9ce42baca85
[your <tw841117@gmail.com>
2023-08-20 12:42:33 -0400

    update PatternCompile

--- PatternCompile.java
+++ PatternCompile.java
@@ -3,6 +3,7 @@ import java.util.regex.Pattern;
 public class PatternCompile {
 	public static void main(String[] args) {
 		
+		// create a regular expression
 		Pattern p=Pattern.compile("\\w+");
 		System.out.println(p.pattern());  // \w+
 	}
