b925ab5bc1ac3771ce22e7926565d0c264fdcd9a
[your <tw841117@gmail.com>
2023-08-22 15:19:37 -0400

    upload PatternSplit

--- PatternSplit.java
+++ PatternSplit.java
@@ -3,7 +3,31 @@ import java.util.regex.Pattern;
 public class PatternSplit {
 	public static void main(String[] args) {
 		
+		
+		// \\s mean space
+		String str1[] = "a b c".split("\\s");  // { "a", "b", "c" }
+		System.out.println(str1[0]);
+		System.out.println(str1[1]);
+		System.out.println(str1[2]);
+		System.out.println();
+		
+		// \\s mean space
+		// + mean matching with multiple times (>=1)
+		String str2[] = "a b  c".split("[\\s]+");  // { "a", "b", "c" }
+		System.out.println(str2[0]);
+		System.out.println(str2[1]);
+		System.out.println(str2[2]);
+		System.out.println();
+		
+		String str3[] = "a,,, b ;; c".split("[\\,\\;\\s]+");  // { "a", "b", "c" }
+		System.out.println(str3[0]);
+		System.out.println(str3[1]);
+		System.out.println(str3[2]);
+		System.out.println();
+		
 		// create a regular expression
+		// d mean matching numbers from 0 to 9
+		// + mean matching with multiple times (>=1)
 		Pattern p=Pattern.compile("\\d+");
 		
 		String[] str = p.split("My name is: Kai My phone:0123456 My Email:helloworld@kai.com");
