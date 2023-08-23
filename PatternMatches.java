3c52d31cfdb71b475191e60d4ddd3ea9ef2b6a5a
[your <tw841117@gmail.com>
2023-08-23 17:01:12 -0400

    upload PatternMatches.java

--- PatternMatches.java
+++ PatternMatches.java
@@ -1,3 +1,4 @@
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class PatternMatches {
@@ -9,5 +10,18 @@ public class PatternMatches {
 		System.out.println(Pattern.matches("\\d+","2223456789"));  // true
 		System.out.println(Pattern.matches("\\d+","2223aa"));  // false
 		System.out.println(Pattern.matches("\\d+","22bb23"));  // false
+		System.out.println();
+		
+		
+		
+		// find keyword having pattern as _o_
+		// m matching characters/underscore/number
+		Pattern p1 = Pattern.compile("\\wo\\w");
+		Matcher matcher = p1.matcher("i dog fox wo od hhh opp ppo and 1o2 ao3");
+//	    System.out.println(matcher.find());  // find
+		while (matcher.find()){
+		    System.out.println(matcher.group()); // dog fox
+		}		
+		System.out.println();
 	}
 }
