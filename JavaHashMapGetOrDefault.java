c8f1c3bacffc4321d62dadc9a56aa2c60e2e1eee
[your <tw841117@gmail.com>
2023-08-28 14:53:30 -0400

    upload JavaHashMapGetOrDefault.java

---
+++ JavaHashMapGetOrDefault.java
@@ -0,0 +1,24 @@
+import java.util.HashMap;
+
+public class JavaHashMapGetOrDefault {
+	public static void main(String[] args) {
+		
+		HashMap<String, String> map = new HashMap<>();
+		
+		map.put("Name", "cookie");
+		map.put("Age", "18");
+		map.put("Sex", "Female");
+		System.out.println(map);
+		
+		// "Name" in the map, so get "cookie"
+		String name = map.getOrDefault("Name", "random");		
+		System.out.println(name);
+		System.out.println(map);
+
+		// "Score" not in the map, so get "80"
+		String score = map.getOrDefault("Score", "80");
+		System.out.println(score);
+		System.out.println(map);
+		
+	}
+}
