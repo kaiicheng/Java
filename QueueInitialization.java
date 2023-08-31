9757a8af0fe58fff41d09d597631eda462a964dd
[your <tw841117@gmail.com>
2023-08-31 12:12:18 -0400

    update QueueInitialization.java

--- QueueInitialization.java
+++ QueueInitialization.java
@@ -7,7 +7,10 @@ public class QueueInitialization {
 		//add()和remove()方法在失败的时候会抛出异常(不推荐)
         Queue<String> queue = new LinkedList<String>();
         
-        // add element
+        // add element to the beginning of the queue
+        queue.add("0");
+        
+        // add element to the end
         queue.offer("a");
         queue.offer("b");
         queue.offer("c");
@@ -18,8 +21,15 @@ public class QueueInitialization {
         }
         System.out.println("");
         
-        // poll() poll (remove) the first element
-        System.out.println("Element polled: "+queue.poll());
+        // remove() removes the first element
+        System.out.println("Element removed: " + queue.remove());
+        for(String q : queue){
+            System.out.print(q + " ");
+        }
+        System.out.println("");
+        
+        // poll() polls (removes) the first element
+        System.out.println("Element polled: " + queue.poll());
         for(String q : queue){
             System.out.print(q + " ");
         }
