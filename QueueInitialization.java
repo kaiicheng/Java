0d655b76f0f4addac1c3dc79f0bbacf7defc0496
[your <tw841117@gmail.com>
2023-08-31 12:06:00 -0400

    upload QueueInitialization.java

---
+++ QueueInitialization.java
@@ -0,0 +1,41 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class QueueInitialization {
+	public static void main(String[] args) {
+
+		//add()和remove()方法在失败的时候会抛出异常(不推荐)
+        Queue<String> queue = new LinkedList<String>();
+        
+        // add element
+        queue.offer("a");
+        queue.offer("b");
+        queue.offer("c");
+        queue.offer("d");
+        queue.offer("e");
+        for(String q : queue){
+            System.out.print(q + " ");
+        }
+        System.out.println("");
+        
+        // poll() poll (remove) the first element
+        System.out.println("Element polled: "+queue.poll());
+        for(String q : queue){
+            System.out.print(q + " ");
+        }
+        System.out.println("");
+        
+        // element() return the first element and not delete
+        System.out.println("Element returned:"+queue.element());
+        for(String q : queue){
+            System.out.print(q + " ");
+        }
+        System.out.println("");
+        
+        // peek() see the first element
+        System.out.println("peek: "+queue.peek());
+        for(String q : queue){
+            System.out.print(q + " ");
+        }
+	}
+}
