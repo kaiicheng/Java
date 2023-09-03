47d4ae5abd8779b6733be2456a88d9955cdd508b
[your <tw841117@gmail.com>
2023-09-02 22:52:01 -0400

    upload BinaryTreePostorderTraversal.java

---
+++ BinaryTreePostorderTraversal.java
@@ -0,0 +1,46 @@
+
+public class BinaryTreePostorderTraversal {
+	
+	public static class TreeNode {
+    	public int value;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int value) {
+            this.value = value;
+        }
+    }
+    
+    public static class Tree {
+        private TreeNode root;
+    }
+    
+    public static void postOrderTraversal(TreeNode root) {
+        if(root == null) {
+            return;
+        }
+        postOrderTraversal(root.left);
+        postOrderTraversal(root.right);
+        System.out.println(root.value);
+    }
+	
+	public static void main(String[] args) {
+		
+	    // create an object of BinaryTree
+	    Tree tree = new Tree();
+	    
+	    // memory space
+//	    System.out.println(tree);  // BinaryTreeInitialization2$Tree@4517d9a3
+
+	    // create nodes of the tree
+	    tree.root = new TreeNode(1);
+
+	    tree.root.left = new TreeNode(2);
+	    tree.root.right = new TreeNode(3);
+	    tree.root.left.left = new TreeNode(4);
+	    tree.root.left.right = new TreeNode(5);
+	    tree.root.right.left = new TreeNode(6);
+	    tree.root.right.right = new TreeNode(7);
+	    
+	    postOrderTraversal(tree.root);
+	}
+}
